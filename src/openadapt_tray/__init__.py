7b4e143335bf355dce9e42e956d3acfb4422408f
semantic-release <semantic-release>
2026-08-27 21:20:21 +0000

    chore: release 0.3.3

--- src/openadapt_tray/__init__.py
+++ src/openadapt_tray/__init__.py
@@ -1,6 +1,6 @@
 """OpenAdapt Tray - System tray application for OpenAdapt."""
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 from openadapt_tray.app import TrayApplication, main
 from openadapt_tray.config import TrayConfig
